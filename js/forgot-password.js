c78ea4ae9a7d4b6a6d5685e2f419e3ae19dae93c
Shehzad <shehzadkhan1564008@gmail.com>
2020-11-10 00:05:33 -0800

    update

--- js/forgot-password.js
+++ js/forgot-password.js
@@ -1,5 +1,19 @@
-$('#forgot-pass .forgot-btn').click(function (e) { 
-    e.preventDefault();
-    $('#forgot-pass').css('display', 'none');
-    $('#forgot-pass-2').css('display', 'block');
+// $('#forgot-pass .forgot-btn').click(function (e) { 
+//     e.preventDefault();
+//     $('#forgot-pass').css('display', 'none');
+//     $('#forgot-pass-2').css('display', 'block');
+// });
+
+$('.forgot-password-page #reset-btn').click(function (e) {
+    const forgotPassword = $('input#mob-number').val().trim();
+    const cond1 = forgotPassword == '';
+
+    if (cond1) {
+        $('.error.forgot-pass').css('display', 'block');
+        $('.error.forgot-pass-2').css('display', 'block');
+    } else {
+        $('#forgot-pass').css('display', 'none');
+        $('#forgot-pass-2').css('display', 'block');
+    }
+
 });
